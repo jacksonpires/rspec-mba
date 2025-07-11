364b6763c48956552dcd4df67bc66ab52167d156
Jackson Pires <jackson.pires@gmail.com>
2025-07-11 17:01:35 -0300

    testes api

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,5 +18,23 @@ RSpec.describe "/users", type: :request do
 
       expect { post(users_url, params:, as: :turbo_stream) }.to change(User, :count).by(1)
     end
+
+    it "creates a new User using JSON" do
+      params = {
+        "user" => {
+          "first_name" => "ABC",
+          "last_name" => "DEF",
+          "email" => "abc@def.com"
+          }
+        }
+
+      post(users_url, params:, as: :json)
+      response_body = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:created)
+      expect(response_body["first_name"]).to eq("ABC")
+      expect(response_body["last_name"]).to eq("DEF")
+      expect(response_body["email"]).to eq("abc@def.com")
+    end
   end
 end
--- test/integration/users_test.rb
+++ test/integration/users_test.rb
@@ -14,4 +14,22 @@ class UsersTest < ActionDispatch::IntegrationTest
       post(users_url, params:, as: :turbo_stream)
     end
   end
+  test "/users.json" do
+    params = {
+    "user" => {
+      "first_name" => "ABC",
+      "last_name" => "DEF",
+      "email" => "abc@def.com"
+      }
+    }
+
+    post(users_url, params:, as: :json)
+
+    response_body = JSON.parse(response.body)
+
+    assert_equal 201, response.status
+    assert_equal "ABC", response_body["first_name"]
+    assert_equal "DEF", response_body["last_name"]
+    assert_equal "abc@def.com", response_body["email"]
+  end
 end
