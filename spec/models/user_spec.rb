bcb7764e10ce108c5a75e709caabae0f7f920e11
Jackson Pires <jackson.pires@gmail.com>
2025-07-15 22:24:41 -0300

    double, mock e stub

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,5 +42,34 @@ RSpec.describe User, type: :model do
       expect(user).to have_attributes(full_name: user.full_name)
       expect(user[:full_name]).to eq(user.full_name)
     end
+
+    it "just a double example" do
+      my_user = double("MyUser", name: "Jackson", admin?: true)
+
+      expect(my_user.name).to eq("Jackson")
+      expect(my_user.admin?).to be(true)
+    end
+
+    it "just a stub example using a model" do
+      user = User.new(first_name: "Jackson", last_name: "Pires")
+      allow(user).to receive(:full_name).and_return("O QUE EU QUISER") # stub
+
+      expect(user.first_name).to eq("Jackson")
+      expect(user.full_name).to eq("O QUE EU QUISER")
+    end
+
+    it "just a stub example using a double" do
+      user = double("User")
+      allow(user).to receive(:full_name).and_return("O QUE EU QUISER") # stub
+
+      expect(user.full_name).to eq("O QUE EU QUISER")
+    end
+
+    it "just a mock example" do
+      user = double("User")
+      expect(user).to receive(:notify).with("Hello!")
+
+      user.notify("Hello!")
+    end
   end
 end
